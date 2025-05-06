7d2cd534a3d9f2459e28aaf2bf423f42ff2e0a1f
Herwans <38727116+Herwans@users.noreply.github.com>
2025-05-07 00:41:55 +0200

    feat: add line selection

---
+++ Hermes.App/Views/Components/DirectoryItem.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Hermes.App.Views.Components
+{
+    /// <summary>
+    /// Logique d'interaction pour DirectoryItem.xaml
+    /// </summary>
+    public partial class DirectoryItem : UserControl
+    {
+        public DirectoryItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
